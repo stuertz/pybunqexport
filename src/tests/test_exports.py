d97b7c17759b0f7c5a0213b75258da45de9fb161
Jan Stürtz <jan@bargten.de>
2019-12-27 21:21:41 +0100

    added tests

---
+++ src/tests/test_exports.py
@@ -0,0 +1,31 @@
+import datetime
+import unittest
+from .. import export
+
+
+class TestExports(unittest.TestCase):
+    def test_flatten(self):
+        self.assertEqual(export._flatten({}), {})
+
+    def test_flatten2(self):
+        res = export._flatten({
+            'a': {
+                'a': 'value',
+                'b': {
+                    'c': None,
+                    'd': 'other_val'
+                }
+            }, 'b': 1})
+        self.assertEqual(res,
+                         {'a_a': 'value',
+                          'a_b_c': None,
+                          'a_b_d': 'other_val',
+                          'b': 1})
+        
+    def test_fmt_date(self):
+        d = "2019-12-23 09:56:48.004134"
+        self.assertEqual(export._fmt_date(d, "%d.%m.%Y"), "23.12.2019")
+
+    def test_fmt_date_empty(self):
+        self.assertEqual(export._fmt_date("", "%d.%m.%Y"), "")
+        
