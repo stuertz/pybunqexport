c762787f6692295dd3ae6debee1cf3ad19976062
Jan Stürtz <jan@bargten.de>
2020-01-08 14:34:32 +0100

    refactored to use more of pandas features, fixes #3

--- bunqexport/export.py
+++ bunqexport/export.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python -W ignore
-import argparse
-import csv
-import datetime
-import json
-import logging
-import sys
-
-import pandas
-
-from bunq.sdk import client
-from bunq.sdk import context
-from bunq.sdk.model import generated
+# -*- coding: utf-8; mode: python -*-
 """
 Connect to bunq api and create a csv file with all latest payments.
 
@@ -20,12 +9,25 @@ even better with the Vorlagen.dat (FM does not support isodates)
 
 """
 
+import argparse
+import json
+import logging
+import sys
+
+import pandas
+
+from bunq.sdk import client
+from bunq.sdk import context
+from bunq.sdk.model import generated
+
 __all__ = ['main']
 
 _log = logging.getLogger(__name__)
 
 
 def _setup_context(conf):
+    """setup the context (login, etc) to work with bunq api"""
+    _log.info("Using conf: %s", conf)
     api_context = context.ApiContext.restore(conf)
     api_context.ensure_session_active()
     api_context.save(conf)
@@ -33,92 +35,105 @@ def _setup_context(conf):
 
 
 def _get_all_payments(count):
+    """Fetch all Payments wie bunq api in bunq_sdk format"""
     pagination = client.Pagination()
     pagination.count = count
-    return generated.endpoint.Payment.list(
+    result = generated.endpoint.Payment.list(
         params=pagination.url_params_count_only).value
+    _log.info('found %d while fetching last %d Payments', len(result), count)
+    return result
 
 
 class Payments():
+    """
+    Abstraction over bunq payments using a pandas dataframe
+    """
     def __init__(self, payments):
         data = (json.loads(p.to_json()) for p in reversed(payments))
         self.payments = pandas.io.json.json_normalize(data)
+        self.payments['created'] = pandas.to_datetime(self.payments['created'])
+        self.payments['updated'] = pandas.to_datetime(self.payments['updated'])
 
-    @classmethod
-    def fmt_date(cls, dateval, fmt):
-        if not dateval:
-            return ""
-        # FIXME: not with py36 :-(
-        # dt = datetime.datetime.fromisoformat(dateval)
-        dt = datetime.datetime.strptime(dateval[:10], "%Y-%m-%d")
-        return dt.strftime(fmt)
+    def __repr__(self):
+        return self.payments.to_string(
+            columns=('created',
+                     'type',
+                     'counterparty_alias.name',
+                     'amount.currency',
+                     'amount.value',
+                     'description'),
+            show_dimensions=False,
+            index=False,
+            justify='left',
+            formatters={
+                'created': lambda x: x.strftime("%d.%m.%Y"),
+                'description': lambda x: x.replace("\n", " ").strip(),
+            })
 
-    def log(self):
-        _log.info("\n%s", self.payments)
-
-    def csv(self, fname, mode):
-        # Create a csv export from bunq data
+    def to_csv(self, fname, mode):
+        """Create a csv export from bunq data"""
         fname += '.csv'
-        with open(fname, 'w', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=self.payments.columns.values)
-            writer.writeheader()
-            for _, row in self.payments.iterrows():
-                if mode == 'lexware':
-                    # FM does not understand ISO Format Timestamps, needs
-                    # DD.MM.YYYY
-                    row['created'] = self.fmt_date(row['created'], "%d.%m.%Y")
-                    row['updated'] = self.fmt_date(row['updated'], "%d.%m.%Y")
-                writer.writerow(dict(row.items()))
+        if mode == 'lexware':
+            self.payments.to_csv(fname, date_format='%d.%m.%Y',
+                                 index=False, line_terminator='\r\n')
+        else:
+            self.payments.to_csv(fname, index=False)
         _log.info("Wrote %s", fname)
 
-    def json(self, fname):
-        # Create a csv export from bunq data
+    def to_json(self, fname):
+        """Create a json export from bunq data"""
         fname += '.json'
-        with open(fname, 'w', newline='') as jsonfile:
-            json.dump([dict(row) for _, row in self.payments.iterrows()],
-                      jsonfile)
+        self.payments.to_json(fname, orient='records', date_format='iso')
         _log.info("Wrote %s", fname)
 
     def __len__(self):
         return len(self.payments)
 
 
-def balances():
-    pagination = client.Pagination()
-    pagination.count = 50
+class Balances():
+    """
+    represent balances of of all active accounts
+    """
+    def __init__(self):
+        pagination = client.Pagination()
+        pagination.count = 50
 
-    all_accounts = generated.endpoint.MonetaryAccountBank.list(
-        pagination.url_params_count_only).value
+        all_accounts = generated.endpoint.MonetaryAccountBank.list(
+            pagination.url_params_count_only).value
 
-    for aa in (monetary_account_bank
-               for monetary_account_bank in all_accounts
-               if monetary_account_bank.status == "ACTIVE"):
-        _log.info(f"Balance: {aa.description} {aa.balance.currency} "
-                  f"{aa.balance.value}")
+        self.balances = {
+            aacc.description: (aacc.balance.currency, aacc.balance.value)
+            for aacc in (monetary_account_bank
+                         for monetary_account_bank in all_accounts
+                         if monetary_account_bank.status == "ACTIVE")}
+
+    def __repr__(self):
+        return "\n".join((f"{k}: {v[1]} {v[0]}"
+                          for k, v in self.balances.items()))
 
 
-def _export(fname, conf, no_of_payments, mode):
-    _log.info("Using conf: %s", conf)
-    _setup_context(conf)
-    user = generated.endpoint.User.get().value.get_referenced_object()
+def _export(fname, payments, user, mode):
+    """Do the exporting in various formats"""
     if fname is None:
-        fname = '%s' % user.id_
-    payments = Payments(_get_all_payments(no_of_payments))
-    _log.info(f'{len(payments)} Payments for {user.display_name}')
-    payments.log()
-    payments.csv(fname, mode)
-    payments.json(fname)
-    balances()
-    context.BunqContext.api_context().save(conf)
+        fname = 'bunq_%s' % user.id_
+    payments.to_csv(fname, mode)
+    payments.to_json(fname)
+
+
+def _print_status(payments):
+    """print some status info to stdout"""
+    print(payments)
+    print(Balances())
 
 
 def main():
+    """main entrypoint"""
     parser = argparse.ArgumentParser()
     parser.add_argument('--conf', default='bunq-sandbox.conf',
                         help='api config file')
     parser.add_argument('--outfile', '-o', default=None,
                         help='name of the export csv file')
-    parser.add_argument('--payments', default=50,
+    parser.add_argument('--payments', default=200,
                         help='Number of payments')
     parser.add_argument('--verbose', '-v', default=False, action='store_true')
     parser.add_argument('--mode', choices=['raw', 'lexware'], default='raw')
@@ -127,7 +142,16 @@ def main():
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO,
                         format="[%(levelname)-7s] %(message)s",
                         stream=sys.stderr)
-    _export(args.outfile, args.conf, args.payments, args.mode)
+    # connect
+    _setup_context(args.conf)
+
+    user = generated.endpoint.User.get().value.get_referenced_object()
+    payments = Payments(_get_all_payments(args.payments))
+    _export(args.outfile, payments, user, args.mode)
+    _print_status(payments)
+
+    # disconnect
+    context.BunqContext.api_context().save(args.conf)
 
 
 if __name__ == '__main__':
--- bunqexport/tests/test_exports.py
+++ bunqexport/tests/test_exports.py
@@ -4,28 +4,5 @@ from .. import export
 
 
 class TestExports(unittest.TestCase):
-    def test_flatten(self):
-        self.assertEqual(export.Payments.flatten({}), {})
-
-    def test_flatten2(self):
-        res = export.Payments.flatten({
-            'a': {
-                'a': 'value',
-                'b': {
-                    'c': None,
-                    'd': 'other_val'
-                }
-            }, 'b': 1})
-        self.assertEqual(res,
-                         {'a_a': 'value',
-                          'a_b_c': None,
-                          'a_b_d': 'other_val',
-                          'b': 1})
-        
-    def test_fmt_date(self):
-        d = "2019-12-23 09:56:48.004134"
-        self.assertEqual(export.Payments.fmt_date(d, "%d.%m.%Y"), "23.12.2019")
-
-    def test_fmt_date_empty(self):
-        self.assertEqual(export.Payments.fmt_date("", "%d.%m.%Y"), "")
-        
+    # tbd
+    pass
